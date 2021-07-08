a2421f091bf561006fcb7cc68ad93cfb52c901a2
Aqsa Amjad <aqsaamjad099@gmail.com>
2021-07-07 19:31:25 -0700

    code review for tailwind

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header class="flex flex-row justify-between space-x-4 bg-white py-6 px-6">
+   <nav class="flex flex-row space-x-6 font-semibold">
+    <a href="#">Home</a>
+    <a href="#">Product</a>
+    <a href="#">Pricing</a>
+    <a href="#">About</a>
+    <a href="#">Contact</a>
+        </nav>
+        </header>
+          </div>
+        
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+class MyHeader extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1 style={{color: "red"}}>Hello Style!</h1>
+      <p>Add a little style!</p>
+      </div>
+    );
+  }
+}
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
